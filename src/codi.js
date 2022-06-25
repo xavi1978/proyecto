f34018e146d114cfe9f17b51db75e2d0bf7f150e
xavi1978 <exa01@protonmail.ch>
2022-06-25 21:52:13 +0200

    2022-06-25

--- src/codi.js
+++ src/codi.js
@@ -1,29 +1,7 @@
 document.getElementById("edit").addEventListener("click", addList);
-
-class Lista {
-	constructor() {
-		this.item1 = document.getElementById("item1").value;
-		this.item2 = document.getElementById("item2").value;
-		this.item3 = document.getElementById("item3").value;
-		this.item4 = document.getElementById("item4").value;
-		this.item5 = document.getElementById("item5").value;
-		this.item6 = document.getElementById("item6").value;
-		this.item7 = document.getElementById("item7").value;
-		this.item8 = document.getElementById("item8").value;
-		this.item9 = document.getElementById("item9").value;
-		this.item10 = document.getElementById("item10").value;
-		this.tema = document.getElementById("tema").value;
-		this.titulo = document.getElementById("titulo").value;
-	}
-}
-
-listaListasGeografia = [];
-listaListasEsport = [];
-listaListasHistoria = [];
-listaListasEconomia = [];
+getTemas();
 
 function addList() {
-	// listaListas + document.getElementById("tema").value.push(new Lista);
 	var asd = [];
 	var listaDeItems = document.getElementsByClassName("item");
 	console.log(listaDeItems);
@@ -32,16 +10,10 @@ function addList() {
 	}
 	console.log(asd);
 
-	// document.body.appendChild("ol");
-
-	// for (let i = 0; i < 10; i++) {
-	// 	document.getElementsByTagName("ol").appendChild(li);
-	// 	document.getElementsByTagName("ol").nthChild(i)=document.getElementById("item" + i).innerHTML;
-	// }
-
 	$.ajax({
-		url: "./codi.php",
+		url: "/src/codi.php",
 		type: "POST",
+		//dataType: "json",
 		data: {
 			api: "insertarItems",
 			tema: document.getElementById("tema").value,
@@ -57,21 +29,87 @@ function addList() {
 			item8: asd[8],
 			item9: asd[9],
 		},
-		// dataType: "json",
+		success: function (response) {
+			console.log(response);
+		},
+		error: function (error) {
+			console.warn("ERROR 2:");
+			console.warn(error);
+		},
+	});
+}
+
+function getTemas() {
+	$.ajax({
+		url: "/src/codi.php",
+		type: "POST",
+		data: {
+			api: "getTemas",
+			// tema: document.getElementById("tema").value,
+			// titulo: document.getElementById("titulo").value,
+		},
+		dataType: "json",
 		success: function (response) {
 			if (response == 0) {
 				console.warn(response);
 			} else {
 				console.log(response);
 				if ("error" in response) {
-					console.warn("ERROR");
+					console.warn("ERROR 1:");
 				} else {
-					console.warn("OK");
+					var selectTema = document.getElementById("tema");
+					response.forEach((element) => {
+						//  console.log(element);
+						var option = document.createElement("option");
+						option.innerHTML = element["tema"];
+						option.value = element["tema"];
+						selectTema.appendChild(option);
+					});
 				}
 			}
 		},
 		error: function (error) {
-			console.warn("ERROR:" + error);
+			console.warn("ERROR 2:");
+			console.warn(error);
+		},
+	});
+}
+
+getTitulos();
+function getTitulos() {
+	$.ajax({
+		url: "/src/codi.php",
+		type: "POST",
+		data: {
+			api: "getTitulos",
+			tema: "Geografia",
+			// titulo: document.getElementById("titulo").value,
+		},
+		dataType: "json",
+		success: function (response) {
+			if (response == 0) {
+				console.warn(response);
+			} else {
+				console.log(response);
+				if ("error" in response) {
+					console.warn("ERROR 1:");
+				} else {
+					// var selectTema = document.getElementById("tema");
+					response.forEach((element) => {
+						//  console.log(element);
+						//  console.log(JSON.parse(element));
+						console.log(element);
+						//   var option = document.createElement("option");
+						//   option.innerHTML = element["tema"];
+						//   option.value = element["tema"];
+						//   selectTema.appendChild(option);
+					});
+				}
+			}
+		},
+		error: function (error) {
+			console.warn("ERROR 2:");
+			console.warn(error);
 		},
 	});
 }
--- src/codi.php
+++ src/codi.php
@@ -1,11 +1,18 @@
 <?php
 switch ($_POST['api']) {
     case "insertarItems":
-        insertarItem($_POST['tema'], $_POST['titulo'], $_POST['item0'], $_POST['item1'], $_POST['item2'], $_POST['item3'], $_POST['item4'], $_POST['item5'], $_POST['item6'], $_POST['item7'], $_POST['item8'], $_POST['item9'],);
-
+        insertarItem($_POST['tema'], $_POST['titulo'], $_POST['item0'], $_POST['item1'], $_POST['item2'], $_POST['item3'], $_POST['item4'], $_POST['item5'], $_POST['item6'], $_POST['item7'], $_POST['item8'], $_POST['item9']);
+        break;
+    case "getTemas":
+        getTemas();
+        break;
+    case "getTitulos":
+        getTitulos($_POST['tema']);
+        break;
+    case "getAllItems":
+        getAllItems();
         break;
     default:
-
         echo "error";
         break;
 }
@@ -19,13 +26,66 @@ switch ($_POST['api']) {
 function insertarItem($tema, $titulo, $item0, $item1, $item2, $item3, $item4, $item5, $item6, $item7, $item8, $item9)
 {
     $conn = new mysqli("localhost", "root", "", "pbd");
-    $sql = "INSERT INTO listas (tema, titulo, item0, item1, item2, item3, item4, item5, item6, item7, item8, item9) VALUES ('" .  $tema . "','" . $titulo . "'," . $item0 .  ",'" .  $item1 . "','" .  $item2 . "','" .  $item3 . "','" .  $item4 . "','" .  $item5 . "','" .  $item6 . "','" .  $item7 . "','" .  $item8 . "','" . $item9 . "');";
-    echo $sql;
+    $sql = "INSERT INTO listas (tema, titulo, item0, item1, item2, item3, item4, item5, item6, item7, item8, item9) VALUES ('" .  $tema . "','" . $titulo . "','" . $item0 .  "','" .  $item1 . "','" .  $item2 . "','" .  $item3 . "','" .  $item4 . "','" .  $item5 . "','" .  $item6 . "','" .  $item7 . "','" .  $item8 . "','" . $item9 . "');";
+    //echo $sql;
     if ($conn->query($sql) === TRUE) {
         echo "Insert OK";
     } else {
-        echo "<br>ERROR";
+        echo "ERROR";
         //echo "Error: insert table \"usuarios\" " . $conn->error . " <br>" . $sql . "<br>";
     }
     $conn->close();
+}
+
+function getTemas()
+{
+    $conn = new mysqli("localhost", "root", "", "pbd");
+    $sql = "SELECT DISTINCT id,tema FROM listas ;";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $arrayTemas = [];
+        while ($row = $result->fetch_assoc()) {
+            array_push($arrayTemas, $row);
+        }
+    }
+    echo json_encode($arrayTemas);
+}
+function getTitulos($tema)
+{
+    $conn = new mysqli("localhost", "root", "", "pbd");
+    $sql = "SELECT id,titulo FROM listas WHERE tema='" . $tema . "';";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $arrayTemas = [];
+        while ($row = $result->fetch_assoc()) {
+            array_push($arrayTemas, $row);
+        }
+    }
+    echo json_encode($arrayTemas);
+}
+function getAllItems()
+{
+    $conn = new mysqli("localhost", "root", "", "pbd");
+    $sql = "SELECT * FROM listas;";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $arrayTemas = [];
+        while ($row = $result->fetch_assoc()) {
+            array_push($arrayTemas, $row);
+        }
+    }
+    echo json_encode($arrayTemas);
+}
+function getItems($tema)
+{
+    $conn = new mysqli("localhost", "root", "", "pbd");
+    $sql = "SELECT * FROM listas WHERE titulo$tema;";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $arrayTemas = [];
+        while ($row = $result->fetch_assoc()) {
+            array_push($arrayTemas, $row);
+        }
+    }
+    echo json_encode($arrayTemas);
 }
